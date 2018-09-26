d5b9e88be0cd4f99be0caf28ff1710bd49399c83
Prem Acharya <prem.acharya@hotmail.com>
2018-09-26 22:19:16 +1000

    multi face detect added

--- src/App.js
+++ src/App.js
@@ -77,7 +77,8 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      // fetch('http://localhost:3000/imageurl', {
+      fetch('http://192.168.99.100:3000/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -87,7 +88,8 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          // fetch('http://localhost:3000/image', {
+          fetch('http://192.168.99.100:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
