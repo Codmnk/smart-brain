5a5836175a039de4ffa15832046092f52e4ef79c
Prem Acharya <prem.acharya@hotmail.com>
2018-10-02 09:24:31 +1000

    JWT added

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './signin.css'
 
 class Signin extends React.Component {
   constructor(props) {
@@ -17,8 +18,11 @@ class Signin extends React.Component {
     this.setState({signInPassword: event.target.value})
   }
 
+  saveAuthTokenSession = (token) => {
+    window.sessionStorage.setItem('token', token)
+  }
+
   onSubmitSignIn = () => {
-    // fetch('http://localhost:3000/signin', {
     fetch('http://192.168.99.100:3000/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
@@ -28,10 +32,26 @@ class Signin extends React.Component {
       })
     })
       .then(response => response.json())
-      .then(user => {
-        if (user.id) {
-          this.props.loadUser(user)
-          this.props.onRouteChange('home');
+      .then(data => {
+        if (data.userId && data.success === 'true') {
+          this.saveAuthTokenSession(data.token)
+          fetch(`http://192.168.99.100:3000/profile/${data.userId}`, {
+            method: 'get',
+            headers: {
+              'Content-Type' : 'application/json',
+              'Authorization': data.token
+            }
+          })
+          .then(resp => resp.json())
+          .then(user => {
+            if(user && user.email){
+              console.log(user)
+              this.props.loadUser(user)
+              this.props.onRouteChange('home')
+            }
+          })
+        .catch(console.log)
+          
         }
       })
   }
@@ -47,7 +67,7 @@ class Signin extends React.Component {
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
                   type="email"
                   name="email-address"
                   id="email-address"
@@ -57,7 +77,7 @@ class Signin extends React.Component {
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                 <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black"
                   type="password"
                   name="password"
                   id="password"
